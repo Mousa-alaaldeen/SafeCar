e93d73f39e849fb7a8ee190d35ff856867f27d9d
Mousa-alaaldeen <azmimousa396@gmail.com>
2024-12-30 14:34:48 +0300

    edite footer

--- resources/views/customer/partial/footer.blade.php
+++ resources/views/customer/partial/footer.blade.php
@@ -4,9 +4,9 @@
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4">Address</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, Jordan</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+962 7 1234 5678</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>safecar@company.com</p>
                     <div class="d-flex pt-2">
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
@@ -22,13 +22,21 @@
                     <p class="mb-0">09.00 AM - 12.00 PM</p>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Services</h4>
+                    <a href="{{route('customer-services')}}" > <h4 class="text-light mb-4"> Services </h4></a4> 
                     <a class="btn btn-link" href="">Diagnostic Test</a>
                     <a class="btn btn-link" href="">Engine Servicing</a>
                     <a class="btn btn-link" href="">Tires Replacement</a>
                     <a class="btn btn-link" href="">Oil Changing</a>
                     <a class="btn btn-link" href="">Vacuam Cleaning</a>
                 </div>
+                <div class="col-lg-3 col-md-6">
+                    <a href="{{route('customer-packages')}}"><h4 class="text-light mb-4">Packages</h4></a>
+                    <a class="btn btn-link" href="">Premium Package</a>
+                    <a class="btn btn-link" href="">Extended Package</a>
+                    <a class="btn btn-link" href="">Regular Package</a>
+                    <a class="btn btn-link" href="">Basic Package</a>
+                </div>
+            </div>
             </div>
         </div> 
     </div>
